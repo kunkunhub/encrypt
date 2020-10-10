4f4a912b4527369f6ae074dc756ad52567e7490e
kunkun <2749320352@qq.com>
2020-10-10 21:10:30 +0800

    python命令行的基本支持

---
+++ core.py
@@ -0,0 +1,71 @@
+"""
+凯撒加密
+"""
+
+import sys
+
+# 加密依据的字符串
+s = """ !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
+
+def ch(x: int)->str:
+    """
+    替代内置函数chr实现更灵活的控制。
+    """
+    return s[x%len(s)]  # 防止越界
+
+def od(c: str)->int:
+    """
+    替代内置函数ord实现更灵活的控制。
+    """
+    return s.find(c)
+
+def check(x: str)->bool:
+    """检查字符串x的值是否合法"""
+    if x == None:
+        return
+    for i in x:
+        if i not in s:
+            print(f"输入不合法：{i}")
+            sys.exit(0)
+
+def encrypt(key: str, m: str):
+    """
+    加密函数
+    传入密钥和明文，转换成密文
+    """
+    # 延长密钥，方便操作
+    tmp = key
+    while len(key) < len(m):
+        key += tmp
+    
+    # 加密功能核心
+    ans = ""
+    for i in range(0, len(m)):  # 遍历明文
+        ans += ch(od(m[i])+od(key[i])+i)    # 最后再加i增加破解难度
+    
+    return ans
+
+def decrypt(key: str, m: str)->str:
+    """
+    解密函数
+    传入密钥和密文，转换成明文。
+    """
+    # 延长密钥，方便操作
+    tmp = key
+    while len(key) < len(m):
+        key += tmp
+    
+    # 加密功能核心
+    ans = ""
+    for i in range(0, len(m)):  # 遍历密文
+        ans += ch(od(m[i])-od(key[i])-i)
+    
+    return ans
+
+if __name__ == "__main__":
+    key = input("请输入密钥：")
+    m = input("请输入明文：")
+    check(key)
+    check(m)
+    print("加密结果:\n", encrypt(key, m))
+    print("解密结果:\n", decrypt(key, m))
